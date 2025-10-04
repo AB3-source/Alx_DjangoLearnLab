d817b0c62580e45054a0322d26e40abc9d40e20f
AB3-source <dinerohardi@gmail.com>
2025-10-04 07:26:18 +0100

    settings updated

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
