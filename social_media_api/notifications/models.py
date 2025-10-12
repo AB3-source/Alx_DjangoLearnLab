8325d8ed7aa80b302ef8715c277a8f3183566371
AB3-source <dinerohardi@gmail.com>
2025-10-12 19:44:59 +0100

    updated notifications models

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    actor = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='actions'
+    )
+    verb = models.CharField(max_length=255)  # e.g., 'liked', 'followed', 'commented'
+
+    # Generic relation to the object the notification is about
+    target_content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    target_object_id = models.CharField(max_length=255, null=True, blank=True)
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"Notification(to={self.recipient}, actor={self.actor}, verb={self.verb})"
